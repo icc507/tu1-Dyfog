b4faa8c953dbb6917a86fc25a65778926d1c6077
Dyfog <111368031+Dyfog@users.noreply.github.com>
2023-08-22 10:44:19 -0400

    Update tu1_p04_producto_i.py

--- tu1_p04_producto_i.py
+++ tu1_p04_producto_i.py
@@ -2,7 +2,7 @@
 #Para una secuencia de números
 #Programe el producto interior (elemento a elemento)
 #de los números en las posiciones pares respecto de las impares
-#(los elementos que faltan deben ser 0) 
+#(los elementos que faltan deben ser 0)
 #-------------------------------------------------------------
 #Ejemplo de Entrada
 # 1 2 3 6 2
@@ -10,5 +10,16 @@
 #Ejemplo de salida 20
 #-------------------------------------
 #Por ahora la lista entrega los elementos pares agregado un cero
-lista = input().split()[0::2]+[0]
-print(lista)
+lista = input().split()
+lista = [int(eval(i)) for i in lista]
+pares = lista[::2]
+impares = lista[1::2]
+if len(pares)< len(impares):
+    pares.append(0)
+elif len(pares) > len(impares):
+    impares.append(0)
+num = 0
+for i in range(len(pares)):
+    num+= pares[i]*impares[i]
+
+print(num)
